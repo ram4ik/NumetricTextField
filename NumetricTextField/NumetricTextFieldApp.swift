5e305247c04e8cbc2a0c76fdbc2127e0eedbae77
Ramill <ramill_ibragimov@icloud.com>
2022-12-26 09:37:39 +0200

    Initial Commit

---
+++ NumetricTextField/NumetricTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumetricTextFieldApp.swift
+//  NumetricTextField
+//
+//  Created by Ramill Ibragimov on 26.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct NumetricTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
